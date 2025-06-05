318e1f2651da584a3428df41132b68ae62c91664
navin kumar <Navin.Kumar@alineahealthcare.in>
2025-06-05 12:08:22 +0530

    Add export functionality for assign cases; implement new routes and controller; update translations for export messages.

--- src/https/medical/index.routes.ts
+++ src/https/medical/index.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import appointmentRoutes from "./medicalCases/routes/dashboard.routes";
 import reportRoutes from "./medicalCases/routes/report.routes";
+import misRoutes from "./medicalCases/routes/mis.routes";
 
 import { verifyToken } from "../../middleware/Auth";
 const router = Router();
 
 router.use("/cases", verifyToken, appointmentRoutes);
 router.use("/report", verifyToken, reportRoutes);
+router.use("/mis", misRoutes);
 
 export default router;
